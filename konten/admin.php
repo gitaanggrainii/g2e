a42e64052559fa5feda81d690546633f5d6cdc7f
Rainitaa <gita@Gita-Anggraini-2.local>
2025-05-31 15:13:45 +0700

    update

--- konten/admin.php
+++ konten/admin.php
@@ -1,48 +1,10 @@
 <?php
 session_start();
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    header("Location: ../login.php");
+    header("Location: login.php");
     exit();
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-    <link rel="stylesheet" href="../css/skincare.css">
-    <script src="https://kit.fontawesome.com/8e0e948c6d.js" crossorigin="anonymous"></script>
-</head>
-<body>
-	<header class="logo-web" style="justify-content: space-between;">
-        <div class="nama-web">
-            <a href="start.php"><h1>G2E</h1></a>
-        </div>
-        <nav class="navbar-menu">
-            <h2>Kelola Produk</h2>
-        </nav>
-        <section class="B">
-            <div class="search-container">
-                <a href="search.php"><img id="search-icon" src="https://img.icons8.com/ios/50/search--v1.png" alt="search--v1"/>
-                <input id="search-bar" type="text" placeholder="Type to search..." /></a>
-            </div>
-            <div class="profil-container">
-                <a href="<?= isset($_SESSION['email']) ? 'profile.php' : 'login.php' ?>">
-                    <img id="user-icon" src="https://img.icons8.com/ios/50/user--v1.png" alt="user--v1" />
-                </a>
-            </div>
-            <div class="cart-container">
-                <a href="keranjang.php"><img id="cart-icon" src="https://img.icons8.com/ios/50/online-shop-shopping-bag.png" alt="online-shop-shopping-bag"/></a>
-                <span>5</span>
-            </div>
-            <div>
-                <div class="favorite-container">
-                  <a href="favorit.php"><img width="20" height="20" src="https://img.icons8.com/ios/50/like--v1.png" alt="like--v1"/></a>
-            </div>
-        </section>
-    </header>
     <form action="simpan_produk.php" method="POST" enctype="multipart/form-data" 
       style="max-width: 500px; margin: 30px auto; padding: 30px; border: 1px solid #ccc; border-radius: 10px; background-color: #f9f9f9; font-family: Arial, sans-serif;">
     
